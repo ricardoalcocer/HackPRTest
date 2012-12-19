56f172b1282a77638aefc885cec6cb6f5f52e875
Ricardo Alcocer <alco@ricardoalcocer.com>
2012-12-18 20:21:02 -0800

    Added sample code to save View as an image so it can later be sent to a web service

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,22 @@ function saveIt(e){
 	// toImage() returns a Blob with a snapshot of whatever was inside it
 	// this blob can be used to create an actual file
 	// for demonstration purposes i'm assigning it to another view
-	$.image5.image=e.source.toImage();
+	//$.image5.image=e.source.toImage();
+
+	// haven't tested this code, but it should write the image to disk
+	// so it can later be used to send to Parse via api call
+	var new_folder = Titanium.Filesystem.getFile(parent, 'test'); 
+
+	if( !new_folder.exists() ) { 
+		new_folder.createDirectory(); }
+	});
+
+	var theImage = e.source.toImage(); 
+	var d=new Date(); 
+	var filename = d.getTime() + ".png"; 
+	var f = Titanium.Filesystem.getFile(parent + '/test',filename); 
+	f.write(theImage);
+
 }
 
 $.index.open();
